a6942ef74ea82390b07c3a162cce57bc08ff22b7
leQwang <s3912105@rmit.edu.vn>
2022-09-01 01:06:03 +0700

    update addItem

--- src/main/java/com/example/videostore/Controller/adminAddItemDialogController.java
+++ src/main/java/com/example/videostore/Controller/adminAddItemDialogController.java
@@ -6,17 +6,22 @@ import com.example.videostore.Model.Item;
 import com.example.videostore.Model.Movie;
 import com.example.videostore.SystemBroker.SingletonDatabase;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-public class adminAddItemDialogController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class adminAddItemDialogController implements Initializable {
     @FXML
     private TextField Title;
 
     @FXML
-    private TextField rentalType;
+    private ComboBox comboBoxRentalType;
 
     @FXML
-    private TextField loanType;
+    private ComboBox comboBoxLoanType;
 
     @FXML
     private TextField numOfCopies;
@@ -25,37 +30,46 @@ public class adminAddItemDialogController {
     private TextField rentalFee;
 
     @FXML
-    private TextField rentalStatus;
+    private ComboBox comboBoxRentalStatus;
 
     @FXML
     private TextField year;
 
     @FXML
-    private TextField genres;
+    private ComboBox comboBoxGenres;
 
     public Item processItem(){
         String title = Title.getText().trim();
-        String RentalType = rentalType.getText().trim();
-        Integer LoanType = Integer.parseInt(loanType.getText().trim());
+        Integer RentalType = comboBoxRentalType.getSelectionModel().getSelectedIndex();
+        Integer LoanType = comboBoxLoanType.getSelectionModel().getSelectedIndex();
         Integer NumOfCopies = Integer.parseInt(numOfCopies.getText().trim());
         Double RentalFee = Double.parseDouble(rentalFee.getText().trim());
-        Boolean RentalStatus = Boolean.parseBoolean(rentalStatus.getText().trim());
+        Boolean RentalStatus = comboBoxRentalStatus.getSelectionModel().getSelectedIndex() == 1 ? true : false;
         String Year = year.getText().trim();
-        Integer Genres = Integer.parseInt(genres.getText().trim());
 
-        if(RentalType.equals("Game")){
-            Item newItem = new Game.GameBuilder().buildTitle(title).buildLoanType(LoanType).buildCopies(NumOfCopies).buildRentalFee(RentalFee).buildRentalStatus(RentalStatus).buildYear(Year).build();
+        Integer Genres = comboBoxGenres.getSelectionModel().getSelectedIndex();
+
+        if(RentalType == 0){
+            Item newItem = new Game.GameBuilder().buildTitle(title).buildLoanType(LoanType).buildCopies(NumOfCopies).buildRentalFee(RentalFee).buildYear(Year).build();
             return newItem;
         }
-        else if (RentalType.equals("DVD")) {
-            Item newItem = new DVD.DVDBuilder().buildTitle(title).buildLoanType(LoanType).buildCopies(NumOfCopies).buildRentalFee(RentalFee).buildRentalStatus(RentalStatus).buildYear(Year).buildGenres(Genres).build();
+        else if (RentalType == 1) {
+            Item newItem = new DVD.DVDBuilder().buildTitle(title).buildLoanType(LoanType).buildCopies(NumOfCopies).buildRentalFee(RentalFee).buildYear(Year).buildGenres(Genres).build();
             return newItem;
         }
-        else if (RentalType.equals("Movie")) {
-            Item newItem = new Movie.MovieBuilder().buildTitle(title).buildLoanType(LoanType).buildCopies(NumOfCopies).buildRentalFee(RentalFee).buildRentalStatus(RentalStatus).buildYear(Year).buildGenres(Genres).build();
+        else if (RentalType == 2) {
+            Item newItem = new Movie.MovieBuilder().buildTitle(title).buildLoanType(LoanType).buildCopies(NumOfCopies).buildRentalFee(RentalFee).buildYear(Year).buildGenres(Genres).build();
             return newItem;
         }
 
         return null;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        comboBoxRentalType.getItems().addAll("Game", "DVD", "Movie");
+        comboBoxLoanType.getItems().addAll("2-day", "1-week");
+        comboBoxRentalStatus.getItems().addAll("Available", "Unavailable");
+        comboBoxGenres.getItems().addAll("Action", "Horror", "Drama", "Comedy");
     }
 }
