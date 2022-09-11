e8f05ee57c422e8d23aa10a391fca54cc76407dc
Devadavid <s3927112@rmit.edu.vn>
2022-09-11 15:49:59 +0700

    up to date

--- src/main/java/com/example/videostore/Controller/CustomerController.java
+++ src/main/java/com/example/videostore/Controller/CustomerController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
