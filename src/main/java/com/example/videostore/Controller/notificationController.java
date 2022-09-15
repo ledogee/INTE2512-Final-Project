bdb35313a3bf50215a43f670f061d02f0ee28864
Dilson <s3863973@rmit.edu.vn>
2022-09-16 01:57:05 +0700

    Update UI

--- src/main/java/com/example/videostore/Controller/notificationController.java
+++ src/main/java/com/example/videostore/Controller/notificationController.java
@@ -2,12 +2,18 @@ package com.example.videostore.Controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -16,9 +22,15 @@ public class notificationController {
     @FXML
     private Label labelNotification;
 
+    @FXML
+    private static DialogPane colorPane;
+
     public void setLabelNotification(String string, String color) {
         labelNotification.setText(string);
         labelNotification.setTextFill(Color.web(color));
+        labelNotification.setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.ITALIC, 24));
+        labelNotification.setAlignment(Pos.CENTER);
+
     }
 
     public static void popAdminNotification(VBox adminVBOX, String string, String color){
@@ -66,10 +78,12 @@ public class notificationController {
         controller.setLabelNotification(string, color);
         Optional<ButtonType> result = dialog.showAndWait();
 
-        if(result.isPresent() && result.get() == ButtonType.OK) {
+        if(result.isPresent() && result.get() == ButtonType.OK)
+        {
 
             System.out.println("Ok pressed");
 
         }
+
     }
 }
